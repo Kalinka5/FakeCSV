df6da8ad1bd5b2069513ff23732c82269846c2fc
Kalinka5 <dania003d@gmail.com>
2023-08-01 16:05:41 +0300

    Updated edit_schema and new_schema templates.
    Now new column card is transparent with white border.

--- FakeCSV/CSV_converter/static/js/edit_schema.js
+++ FakeCSV/CSV_converter/static/js/edit_schema.js
@@ -4,8 +4,6 @@ let columnFormsContainer = document.querySelector('#columns-container');
 let formCount = document.querySelectorAll('.card-body').length;
 
 addColumnButton.addEventListener('click', function() {
-    formCount++;
-
     let columnForm = document.createElement('div');
     columnForm.className = 'card-body';
 
@@ -14,17 +12,17 @@ addColumnButton.addEventListener('click', function() {
         <div class="row">
         <div class="col">
             <label for="column_name${formCount}" class="form-label">Column name</label>
-            <input type="text" id="column_name${formCount}" name="column_name" class="form-control">
+            <input type="text" id="column_name${formCount}" name="column_name" class="form-control" placeholder="e.g. Employers">
         </div>
         <div class="col">
             <label for="column_separator${formCount}" class="form-label">Type</label>
             <select class="form-select" name="type" id="column_separator${formCount}" aria-label="Default select example">
-            <option selected>Open this select menu</option>
-            <option value="1">Full name</option>
-            <option value="2">Job</option>
-            <option value="3">Email</option>
-            <option value="4">Integer</option>
-            <option value="5">Date</option>
+                <option selected>------</option>
+                <option value="1">Full name</option>
+                <option value="2">Job</option>
+                <option value="3">Email</option>
+                <option value="4">Integer</option>
+                <option value="5">Date</option>
             </select>
         </div>
         <div class="col">
@@ -45,7 +43,7 @@ addColumnButton.addEventListener('click', function() {
         </div>
         <div class="col">
             <label for="order${formCount}" class="form-label">Order</label>
-            <input id="order${formCount}" type="text" class="form-control" name="order">
+            <input id="order${formCount}" type="text" class="form-control" name="order" value="${formCount}">
         </div>
         <div class="col">
         <button type="button" class="btn btn-outline-danger special">Delete</button>
@@ -60,6 +58,8 @@ addColumnButton.addEventListener('click', function() {
 
     let typeSelect = columnForm.querySelector(`#column_separator${formCount}`);
     let integerFields = columnForm.querySelector(`#integerFields${formCount}`);
+
+    formCount++;
 
     // Hide the "From" and "To" fields initially
     integerFields.classList.add('hidden-fields');
