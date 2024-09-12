1ce05aaf8e9c53ead6da8771b137180c0f346c49
Kalinka5 <dania003d@gmail.com>
2024-09-12 15:29:10 +0300

    Add tests to views

--- FakeCSV/tests/test_views.py
+++ FakeCSV/tests/test_views.py
@@ -3,11 +3,16 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from django.http import JsonResponse
 
-from CSV_converter.models import Schema, Column
+from faker import Faker
+
+from CSV_converter.models import Schema, Column, IntegerColumn
+from CSV_converter.views import get_email, generate_fake_data
+
 
 class ViewTests(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.user = User.objects.create_user(
+            username='testuser', password='testpassword')
 
         self.client = Client()
         self.client.login(username='testuser', password='testpassword')
@@ -18,12 +23,57 @@ class ViewTests(TestCase):
             column_separator=',',
             string_character='"'
         )
-        self.column = Column.objects.create(
+        self.column1 = Column.objects.create(
             name='ColumnName',
             type='1',
             order='1',
             schema=self.schema
         )
+        self.column2 = Column.objects.create(
+            name='ColumnName2',
+            type='2',
+            order='2',
+            schema=self.schema
+        )
+        self.column3 = Column.objects.create(
+            name='ColumnName3',
+            type='3',
+            order='3',
+            schema=self.schema
+        )
+        self.column4 = IntegerColumn.objects.create(
+            name='ColumnName4',
+            type='4',
+            order='4',
+            schema=self.schema
+        )
+        self.column5 = Column.objects.create(
+            name='ColumnName5',
+            type='5',
+            order='5',
+            schema=self.schema
+        )
+        self.faker = Faker()
+
+    def test_get_email_type(self):
+        email = get_email(self.faker)
+        self.assertIsInstance(email, str)
+
+    def test_generate_fake_data_type(self):
+        columns = [self.column1]
+        fake_data = generate_fake_data(columns, self.faker)
+        self.assertIsInstance(fake_data, list)
+
+    def test_generate_fake_data_length_one_item(self):
+        columns = [self.column1]
+        fake_data = generate_fake_data(columns, self.faker)
+        self.assertEqual(1, len(fake_data))
+
+    def test_generate_fake_data_length_5_items(self):
+        columns = [self.column1, self.column2,
+                   self.column3, self.column4, self.column5]
+        fake_data = generate_fake_data(columns, self.faker)
+        self.assertEqual(5, len(fake_data))
 
     def test_data_schemas_view_get(self):
         url = reverse('data_schemas')
@@ -35,7 +85,8 @@ class ViewTests(TestCase):
         data = {
             'schema_id': '1',
         }
-        response = self.client.post(url, data, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        response = self.client.post(
+            url, data, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
         self.assertIsInstance(response, JsonResponse)
         self.assertTrue(response.json()['success'])
 
@@ -64,7 +115,8 @@ class ViewTests(TestCase):
             'from[]': '10',
             'to[]': '70'
         }
-        response = self.client.post(url, data, follow=True) # follow=True to follow redirects
+        # follow=True to follow redirects
+        response = self.client.post(url, data, follow=True)
         self.assertEqual(response.status_code, 200)
 
     def test_data_sets_view_get(self):
